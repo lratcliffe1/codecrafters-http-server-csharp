061c6e34b16a69ee63f216ba2e86ccd725f84f1a
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2026-02-10 11:55:53 +0100

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,31 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-// TODO: Uncomment the code below to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+  TcpClient client = server.AcceptTcpClient();
+  Thread clientThread = new(() => HandleClient(client));
+  clientThread.Start();
+}
+
+static void HandleClient(TcpClient client)
+{
+  using TcpClient _ = client;
+  byte[] bytes = new byte[256];
+  string? data = null;
+  NetworkStream stream = client.GetStream();
+
+  int i;
+  while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+  {
+    data = Encoding.ASCII.GetString(bytes, 0, i);
+
+    byte[] msg = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+    stream.Write(msg, 0, msg.Length);
+    stream.Flush();
+  }
+}
